09f9c50e36ddccc2bd82df9699c0485610cc9d3b
punalsc <punalsc@gmail.com>
2017-09-01 12:28:40 +0100

    Revert "Feature/fixed typeof undefined"

--- breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<div id="breadcrumb-holder" class="tna-breadcrumb">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <?php if (function_exists('dimox_breadcrumb')) dimox_breadcrumb(); ?>
-            </div>
-        </div>
-    </div>
-</div>
--- js/tests/profile-page-tests.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by mdiaconita on 31/05/2017.
- */
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
-});
